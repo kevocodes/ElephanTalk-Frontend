2dbeb08898d563a87655c0d7fdd823210471eac5
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-11-01 12:53:52 -0600

    feat add cardHeader content, the application name and a small comment:construction:

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -15,7 +15,16 @@ export default function Login() {
           className="border-none bg-background/60 dark:bg-default-100/50 w-full"
           shadow="sm"
         >
-          <CardHeader className="flex gap-3"></CardHeader>
+          <CardHeader className="flex gap-3">
+            <div className="flex flex-col pl-2">
+              <p className="text-md text-primary text-3xl font-bold mb-2  sm:text-lef ">
+                ElephanTalk
+              </p>
+              <p className="text-small text-default-500 text-primary-50">
+                Connect with phantastic people
+              </p>
+            </div>
+          </CardHeader>
           <Divider />
           <CardBody></CardBody>
 
