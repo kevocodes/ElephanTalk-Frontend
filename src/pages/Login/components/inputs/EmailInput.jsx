cfb99b5d263210c1a191c7a4d650c02f4ee97489
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-11-01 13:06:52 -0600

    feat(EmailInput): import UI component and add input structure:sparkles:

---
+++ src/pages/Login/components/inputs/EmailInput.jsx
@@ -0,0 +1,17 @@
+import { Input } from '@nextui-org/react'
+
+export default function App({ onChange, value }) {
+  return (
+    <Input
+      isClearable
+      type="email"
+      label="Username or Email"
+      variant="bordered"
+      color="primary-50"
+      onClear={() => console.log('input cleared')}
+      className="w-full text-primary-50 pt-4 pb-2"
+      onChange={onChange}
+      value={value}
+    />
+  )
+}
