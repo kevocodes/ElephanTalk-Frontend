403739b1ab40f2133dca0eb28ab2637e691d35e8
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-11-01 14:07:28 -0600

    feat(input): add UI input structure and eye icon functionality:construction:

--- src/pages/Login/components/inputs/PasswordInput.jsx
+++ src/pages/Login/components/inputs/PasswordInput.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import {Input} from "@nextui-org/react";
-import { EyeFilledIcon } from "../icons/EyeFilledIcon";
-import { EyeSlashFilledIcon } from "../icons/EyeSlashFilledIcon";
+import React from 'react'
+import { Input } from '@nextui-org/react'
+import { EyeFilledIcon } from '../icons/EyeFilledIcon'
+import { EyeSlashFilledIcon } from '../icons/EyeSlashFilledIcon'
 
-export default function PasswordInput({onChange,value}){
+export default function PasswordInput({ onChange, value }) {
+  const [isVisible, setIsVisible] = React.useState(false)
 
-    return(
-        
-    )
+  const toggleVisibility = () => setIsVisible(!isVisible)
+  return (
+    <Input
+      label="Password"
+      variant="bordered"
+      color="primary-50"
+      endContent={
+        <button
+          className="focus:outline-none"
+          type="button"
+          onClick={toggleVisibility}
+        >
+          {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )}
+        </button>
+      }
+      type={isVisible ? 'text' : 'password'}
+      className="w-full pt-4  "
+      onChange={onChange}
+      value={value}
+    />
+  )
 }
