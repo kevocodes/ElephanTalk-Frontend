b2e5270ae6f2236299e8ef9272528be1915ba62c
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-11-01 14:01:43 -0600

    feat(input): import input component and icons to use:construction:

---
+++ src/pages/Login/components/inputs/PasswordInput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Input} from "@nextui-org/react";
+import { EyeFilledIcon } from "../icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "../icons/EyeSlashFilledIcon";
+
+export default function PasswordInput({onChange,value}){
+
+    return(
+        
+    )
+}
