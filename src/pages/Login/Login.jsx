47075488a240731828fb74b16461d8f675cd4f5a
MarcBgF05 <62720789+MarcBgF05@users.noreply.github.com>
2023-11-01 12:50:12 -0600

    feat: add base card component for the form:sparkles:

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,27 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Divider,
-  } from '@nextui-org/react'
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+} from '@nextui-org/react'
 
-export default function Login(){
+export default function Login() {
+  return (
+    <main>
+      <div>
+        <Card
+          isBlurred
+          className="border-none bg-background/60 dark:bg-default-100/50 w-full"
+          shadow="sm"
+        >
+          <CardHeader className="flex gap-3"></CardHeader>
+          <Divider />
+          <CardBody></CardBody>
 
-    return(
-        <main>
-            <div>
-                
-            </div>
-        </main>
-    )
+          <CardFooter></CardFooter>
+        </Card>
+      </div>
+    </main>
+  )
 }
