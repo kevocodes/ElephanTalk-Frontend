61f01a12557af1ed072e26062df17fb75e9b6f9a
kikicodes23 <00117420@uca.edu.sv>
2023-11-01 23:33:20 -0600

    feat: create each action for the section :sparkles:

---
+++ src/pages/Home/components/Post/ActionsControllers/ControllerToggle/ControllerToggle.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@nextui-org/react";
+
+function ControllerToggle({ state, action, Component }) {
+  return (
+    <Button
+      isIconOnly
+      className="text-default-500 data-[hover]:bg-foreground/10"
+      radius="full"
+      variant="light"
+      onPress={() => action()}
+    >
+      <Component
+        className={state ? "[&>path]:stroke-transparent" : ""}
+        fill={state ? "currentColor" : "none"}
+      />
+    </Button>
+  );
+}
+
+export default ControllerToggle;
