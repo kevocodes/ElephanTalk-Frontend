053aa284318f28bd607f77739f7a716acef1c662
fmel025 <00018720@uca.edu.sv>
2023-11-03 14:20:02 -0600

    feat: add initial state value for post form :construction:

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,5 +1,5 @@
 /* eslint no-unused-vars: "warn"*/
-
+import { useState } from "react";
 import {
   Card,
   CardHeader,
@@ -7,17 +7,40 @@ import {
   CardFooter,
   Divider,
   Image,
-  Input
+  Input,
 } from "@nextui-org/react";
 
-function PostForm({ title = "Create New Post", data = {}, action }) {
+function PostForm({
+  title = "Create New Post",
+  description = "",
+  image = "",
+  action,
+}) {
+  const [formValues, setFormValues] = useState(() => {
+    // The function returns the initial state value
+    return {
+      description: description,
+      image: image,
+    };
+  });
+
   return (
     <Card className="mx-auto mt-4 max-w-[80%]">
       <CardHeader className="font-oswald text-3xl">{title}</CardHeader>
       <Divider />
       <CardBody className="flex gap-4">
-        <Input name="description" label="Descripcion" isRequired={true} />
-        <Input name="image" label="URL de la imagen" isRequired={true} />
+        <Input
+          name="description"
+          value={formValues.description}
+          label="Descripcion"
+          isRequired={true}
+        />
+        <Input
+          name="image"
+          value={formValues.image}
+          label="URL de la imagen"
+          isRequired={true}
+        />
       </CardBody>
     </Card>
   );
