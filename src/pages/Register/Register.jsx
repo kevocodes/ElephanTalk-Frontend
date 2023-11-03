1de63bba1957a8a78f6e04e8e1ec359594f9dfda
Samuel Ortiz <00023020@uca.edu.sv>
2023-11-03 17:31:37 -0600

    feat: create base card for login :sparkles:

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -16,8 +16,31 @@ import RegisterButton from './components/buttons/RegisterButton'
 
 function Register() {
     return (
-        <>
-        </>
+        <main className='h-screen grid justify-items-center bg-login  bg-cover bg-center bg-no-repeat'>
+            <div className='flex flex-col justify-center lg:h-full md:w-96 sm:mx-2 md:m-auto sm:m-auto sm:my-8 sm:h-80'>
+                <Card
+                    isBlurred
+                    className="border-none bg-background/60 dark:bg-default-100/50 w-full"
+                    shadow="sm">
+                    <CardHeader className="flex gap-3 flex-col">
+                        <img
+                            alt="logo logo"
+                            height={80}
+                            radius="sm"
+                            src={logo}
+                            width={220}
+                        />
+                        <div className="flex flex-col pl-2">
+                            <p className="text-small text-default-500 text-foreground">
+                                Connect with phantastic people
+                            </p>
+                        </div>
+                    </CardHeader>
+                    <Divider />
+
+                </Card>
+            </div>
+        </main>
     )
 }
 
