8459070552ff44ddc945eacb54b500575683dfc7
fmel025 <00018720@uca.edu.sv>
2023-11-03 12:18:52 -0600

    feat: add base post form design :sparkles:

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,9 +1,26 @@
 /* eslint no-unused-vars: "warn"*/
 
-function PostForm({ title = "Create new post", data = {} , action }) {
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Image,
+  Input
+} from "@nextui-org/react";
+
+function PostForm({ title = "Create New Post", data = {}, action }) {
   return (
-    <div>PostForm</div>
-  )
+    <Card className="mx-auto mt-4 max-w-[80%]">
+      <CardHeader className="font-oswald text-3xl">{title}</CardHeader>
+      <Divider />
+      <CardBody className="flex gap-4">
+        <Input name="description" label="Descripcion" isRequired={true} />
+        <Input name="image" label="URL de la imagen" isRequired={true} />
+      </CardBody>
+    </Card>
+  );
 }
 
-export default PostForm
+export default PostForm;
