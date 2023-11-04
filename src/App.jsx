ed0ad268b8b0bfe2f9c3717d0eec12971a32f2d4
kikicodes23 <00117420@uca.edu.sv>
2023-11-04 00:08:00 -0600

    feat: add own posts view functionality :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Favorites from "./pages/Favorites/Favorites";
+import Own from "./pages/Own/Own";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Home />} />
             <Route path="/favorites" element={<Favorites />} />
-            <Route path="/own" element={<h1>Own posts</h1>} />
+            <Route path="/own" element={<Own />} />
             <Route path="/post/:id" element={<h1>Post</h1>} />
             <Route path="/edit/:id" element={<h1>Edit post</h1>} />
             <Route path="/create" element={<h1>Create post</h1>} />
---
+++ src/pages/Own/Own.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { getPosts, toggleFavoritePost, toggleLikePost } from "../../services/posts.service";
+import { useAuth } from "../../utils/tempUser";
+import Post from "../Home/components/Post/Post";
+
+function Own() {
+  const [posts, setPosts] = useState([]);
+
+  const { token, user } = useAuth();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await getPosts({ token, endpoint: "owned" });
+        setPosts(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchPosts();
+  }, [setPosts, token]);
+
+  const handleLike = async ({ setLiked, liked, setLikes, postId }) => {
+    try {
+      // Update the UI first for better UX
+      setLiked((v) => !v);
+
+      setLikes((prevLikes) => {
+        if (!liked) return [...prevLikes, user];
+
+        return prevLikes.filter((like) => like._id !== user._id);
+      });
+
+      // Send the request to the server
+      await toggleLikePost({ token, postId });
+    } catch (error) {
+      // If the request fails, set the UI to the previous state
+      setLiked((v) => !v); // toggle the like state
+
+      setLikes((prevLikes) => {
+        // Undo the like
+        if (liked) return [...prevLikes, user];
+
+        return prevLikes.filter((like) => like._id !== user._id);
+      });
+
+      console.log(error);
+    }
+  };
+
+  const handleFavorite = async ({ setFavorited, postId }) => {
+    try {
+      // Update the UI first for better UX
+      setFavorited((v) => !v);
+
+      // Send the request to the server
+      await toggleFavoritePost({ token, postId });
+    } catch (error) {
+      // If the request fails, set the state to the previous value
+      setFavorited((v) => !v);
+      console.log(error);
+    }
+  };
+
+  return (
+    <main className="flex flex-col gap-4 items-center py-4">
+      {posts.map((post) => (
+        <Post
+          key={post._id}
+          info={post}
+          setPosts={setPosts}
+          onLike={handleLike}
+          onFavorite={handleFavorite}
+        />
+      ))}
+    </main>
+  );
+}
+
+export default Own;
