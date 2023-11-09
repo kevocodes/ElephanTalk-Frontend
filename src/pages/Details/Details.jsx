f59ef1c154f88c2964085b78b2059c732b909d06
ChristopherCalderon <chris2001289@gmail.com>
2023-11-09 00:48:57 -0600

    feat: fetch basic post information :sparkles:

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -6,7 +6,14 @@ import {
   CardFooter,
   Image,
 } from "@nextui-org/react";
-import { useRef } from "react";
+import {
+  getPosts,
+  toggleFavoritePost,
+  toggleLikePost,
+} from "../../services/posts.service";
+
+import { useAuth } from "../../utils/tempUser";
+import { useRef, useState, useEffect } from "react";
 import CommentCard from "./components/CommentCard/CommentCard";
 import ActionsControllers from "../../components/ActionsControllers/ActionsControllers";
 import InteractionsDetails from "../../components/InteractionsDetails/InteractionsDetails";
@@ -16,25 +23,44 @@ import CommentSection from "./components/CommentSection/CommentSection";
 
 function Details() {
   const inputRef = useRef(null);
+  const postId = window.location.pathname.split('/').pop();
+  const { token, user } = useAuth();
+  const [post, setPost] = useState('')
+  const [postLikes, setLikes] = useState();
 
   const handleComment = () => {
     inputRef.current.focus();
   };
+
+  const getData = async () => {
+    try {
+      console.log(postId);
+      let response = await getPosts({ token, endpoint: postId });
+      if (response) {
+        console.log(response);
+        setPost(response.data);
+        setLikes(response.data.likes);
+        console.log();
+      } 
+    } catch (error) {
+      console.error('Error al obtener datos de la API:', error);
+    }
+};
+
+useEffect(() => {
+  getData();
+}, []);
+
 
   return (
     <main className="flex-1 absolute top-0 py-14 lg:pb-0 flex flex-col justify-center items-center w-full h-screen">
       <Card className="lg:w-10/12 lg:h-full lg:my-5 w-full h-full ">
         <CardHeader className="justify-between px-5 mt-2">
           <div className="flex gap-5">
-            <Avatar
-              isBordered
-              radius="full"
-              size="md"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-            />
+          <Avatar isBordered radius="full" size="md" src={post ? post.user.picture : ''} />
             <div className="flex flex-col gap-1 items-start justify-center">
-              <p className="text-small font-semibold leading-none">Hola</p>
-              <p className="text-small tracking-tight">@Hola</p>
+              <p className="text-small font-semibold leading-none">{post ? `${post.user.name} ${post.user.lastname}` : ''}</p>
+              <p className="text-small tracking-tight">{post ? `@${post.user.username}` : ''}</p>
             </div>
           </div>
         </CardHeader>
@@ -43,13 +69,13 @@ function Details() {
             <Image
               alt="Card background "
               className="object-cover rounded-xl w-full"
-              src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+              src={post ? post.image : ''}
             />
           </div>
           <div className="lg:w-1/2 flex flex-col h-full lg:overflow-hidden lg:mt-0 mt-2 gap-3 lg:px-2 lg:pb-2">
-            <ActionsControllers onComment={handleComment} />
-            <PostDetails description="Descripcion default para pruebas" />
-            <InteractionsDetails onComment={handleComment}/>
+            <ActionsControllers onComment={handleComment}   />
+            <PostDetails description={post ? post.description : ''} />
+            <InteractionsDetails onComment={handleComment} likes={post.likes} comments={post ? post.comments.length : ''}/>
             <div className="w-full lg:order-5">
               <CommentForm inputRef={inputRef} />
             </div>
