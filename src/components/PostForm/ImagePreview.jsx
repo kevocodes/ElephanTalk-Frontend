b92eff61c56bbe3c663d68deaf7cdfa233c12056
fmel025 <00018720@uca.edu.sv>
2023-11-10 21:49:05 -0600

    feat: add states for image preview functionality :sparkles:

--- src/components/PostForm/ImagePreview.jsx
+++ src/components/PostForm/ImagePreview.jsx
@@ -1,7 +1,12 @@
 import { Image } from "@nextui-org/react"
 import noPreview from "../../assets/no-preview.png"
+import { useState, useEffect } from "react";
 
 function ImagePreview({ image }) {
+  
+  const [isImageValid, setIsImageValid] = useState(true);
+  const [imageSrc, setImageSrc] = useState("");
+
   return (
     <div className="flex flex-col gap-2 py-4">
       <h1 className="text-xl text-left w-full font-bold">Preview:</h1>
