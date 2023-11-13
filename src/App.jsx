ff216224f31ca61a06312570658c7e4053ff5d99
fmel025 <00018720@uca.edu.sv>
2023-11-12 19:14:32 -0600

    feat: add the edit post page component to the router :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import MainLayout from "./components/MainLayout/MainLayout";
 import AppProvider from "./providers/app.provider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreatePost from "./pages/CreatePost/CreatePost";
+import UpdatePost from "./pages/UpdatePost/UpdatePost";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
             <Route path="/favorites" element={<h1>Favorite posts</h1>} />
             <Route path="/own" element={<h1>Own posts</h1>} />
             <Route path="/post/:id" element={<h1>Post</h1>} />
-            <Route path="/edit/:id" element={<h1>Edit post</h1>} />
+            <Route path="/edit/:id" element={<UpdatePost />} />
             <Route path="/create" element={<CreatePost />} />
             <Route path="*" element={<h1>404</h1>} />
           </Route>
--- src/pages/UpdatePost/UpdatePost.jsx
+++ src/pages/UpdatePost/UpdatePost.jsx
@@ -2,6 +2,7 @@ import PostForm from "../../components/PostForm/PostForm";
 
 function UpdatePost() {
   // Here the logic will change due to the fact we are updating the post
+  // Like getting the id and stuff
   return (
     <PostForm
       title="Update Post"
