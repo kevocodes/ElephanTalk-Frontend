d5fd821e261148f08127688b281340eb3922286e
fmel025 <00018720@uca.edu.sv>
2023-11-12 19:10:56 -0600

    feat: create UpdatePost component :sparkles:

---
+++ src/pages/UpdatePost/UpdatePost.jsx
@@ -0,0 +1,14 @@
+import PostForm from "../../components/PostForm/PostForm";
+
+function UpdatePost() {
+  // Here the logic will change due to the fact we are updating the post
+  return (
+    <PostForm
+      title="Update Post"
+      description="dummy description"
+      image="dummy image"
+    />
+  );
+}
+
+export default UpdatePost;
