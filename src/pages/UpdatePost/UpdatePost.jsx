e1d61990e8957bd80733a2b009dfd8861e91b324
fmel025 <00018720@uca.edu.sv>
2023-11-13 09:52:27 -0600

    feat: implement function to fetch post data :sparkles:

--- src/pages/UpdatePost/UpdatePost.jsx
+++ src/pages/UpdatePost/UpdatePost.jsx
@@ -1,7 +1,9 @@
 import PostForm from "../../components/PostForm/PostForm";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useAuthStore } from "../../store/auth.store"
+import { useAuthStore } from "../../store/auth.store";
+import { getPosts, updatePost } from "../../services/posts.service";
+import { showAlert } from "../../utils/toastify.util";
 
 function UpdatePost() {
   const [isLoading, setIsLoading] = useState(true);
@@ -11,8 +13,23 @@ function UpdatePost() {
   const token = useAuthStore((state) => state.token);
 
   useEffect(() => {
-    console.log(id);
-    setIsLoading(false);
+    const fetchPost = async () => {
+      try {
+        const response = await getPosts({ token: token, endpoint: id });
+        if (response) {
+          setDescription(response.data.description);
+          setImage(response.data.image);
+        }
+      } catch (error) {
+        showAlert("Oops try again later...", "error");
+      } finally {
+        // Either it was successful or not
+        // We want to stop the loading process
+        setIsLoading(false);
+      }
+    };
+
+    fetchPost();
   }, []);
 
   return (
@@ -20,8 +37,8 @@ function UpdatePost() {
       {!isLoading && (
         <PostForm
           title="Update Post"
-          description="dummy description"
-          image="dummy image"
+          description={description}
+          image={image}
         />
       )}
     </>
