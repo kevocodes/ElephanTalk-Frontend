890326d49f25b5f9963b0c8aab43c4338480d385
kevocodes <00116120@uca.edu.sv>
2023-11-24 21:56:05 -0600

    feat: add on clic handler to logo in header :sparkles:

--- src/components/MainLayout/NavigationBar/BrandSection/BrandSection.jsx
+++ src/components/MainLayout/NavigationBar/BrandSection/BrandSection.jsx
@@ -1,11 +1,21 @@
 import { Image, NavbarBrand, NavbarContent } from "@nextui-org/react";
 import logo from "../../../../assets/logo-letters.webp";
+import { useNavigate } from "react-router-dom";
 
 function BrandSection() {
+  const navigate = useNavigate();
+
   return (
     <NavbarContent>
       <NavbarBrand>
-        <Image src={logo} height={100} width={145} radius="none" />
+        <Image
+          src={logo}
+          height={100}
+          width={145}
+          className="cursor-pointer"
+          radius="none"
+          onClick={() => navigate("/")}
+        />
       </NavbarBrand>
     </NavbarContent>
   );
